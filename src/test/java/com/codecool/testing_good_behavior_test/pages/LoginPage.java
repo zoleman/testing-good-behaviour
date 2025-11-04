41f2b74f9767eae98989f53d831d98d31e39bd6a
csengerthecoder <191019948+csengerthecoder@users.noreply.github.com>
2025-11-04 10:05:58 +0100

    merging

---
+++ src/test/java/com/codecool/testing_good_behavior_test/pages/LoginPage.java
@@ -0,0 +1,4 @@
+package com.codecool.testing_good_behavior_test.pages;
+
+public class LoginPage {
+}
---
+++ src/test/java/com/codecool/testing_good_behavior_test/tests/LoginPageTest.java
@@ -0,0 +1,4 @@
+package com.codecool.testing_good_behavior_test.tests;
+
+public class LoginPage {
+}
