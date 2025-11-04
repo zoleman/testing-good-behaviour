df8fe2969bf47cca6dfa3f7b7e7dd0c9b5c905e8
Zoltan Imre <pugomagitor@gmail.com>
2025-11-04 12:15:08 +0100

    fixed added to basetest also

--- src/test/java/com/codecool/testing_good_behavior_test/base/BaseTest.java
+++ src/test/java/com/codecool/testing_good_behavior_test/base/BaseTest.java
@@ -2,14 +2,17 @@ package com.codecool.testing_good_behavior_test.base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@Execution(ExecutionMode.SAME_THREAD)
 public abstract class BaseTest {
     protected WebDriver driver;
     protected static Dotenv dotenv;
